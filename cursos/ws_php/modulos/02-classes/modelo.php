ef88b1289e574faa0f2ace1b8d2ea73b2e9110ca
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-02-26 23:10:41 -0300

    Carga Automatica

---
+++ cursos/ws_php/modulos/02-classes/modelo.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>WS PHP - Carga Automática</title>
+    </head>
+    <body>
+        <?php
+
+        ?>
+    </body>
+</html>
